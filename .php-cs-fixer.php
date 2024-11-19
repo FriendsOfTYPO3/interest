9b3413ab14e9a11e78ecd2443a836a015d28d6c1
mabolek <git@mabolek.dev>
2024-11-19 16:25:30 +0100

    [TASK] Update CS Fixer rules

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,6 +1,8 @@
 <?php
 
 /** @noinspection PhpUndefinedNamespaceInspection */
-$config = \TYPO3\CodingStandards\CsFixerConfig::create();
+$config = \TYPO3\CodingStandards\CsFixerConfig::create()->addRules([
+    'single_line_empty_body' => false,
+]);
 $config->getFinder()->in('Classes')->in('Configuration')->in('Tests');
 return $config;
