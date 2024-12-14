6b23be87968faa4ce1568b8568c5e3eb7e7df76f
mabolek <git@mabolek.dev>
2024-12-14 15:23:50 +0100

    [TASK] Add new reaction configuration for impersonate user

--- Configuration/TCA/Overrides/sys_reaction.php
+++ Configuration/TCA/Overrides/sys_reaction.php
@@ -12,4 +12,27 @@ if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('reactions')) {
             'icon' => \Pixelant\Interest\Reaction\CreateUpdateDeleteReaction::getIconIdentifier(),
         ]
     );
+
+    $GLOBALS['TCA']['sys_reaction']['ctrl']['typeicon_classes'][\Pixelant\Interest\Reaction\CreateUpdateDeleteReaction::getType()] = \Pixelant\Interest\Reaction\CreateUpdateDeleteReaction::getIconIdentifier();
+
+    $GLOBALS['TCA']['sys_reaction']['palettes']['interestCreateUpdateDelete'] = [
+        'label' => 'LLL:EXT:reactions/Resources/Private/Language/locallang_db.xlf:palette.additional',
+        'showitem' => 'impersonate_user',
+    ];
+
+    $GLOBALS['TCA']['sys_reaction']['types'][\Pixelant\Interest\Reaction\CreateUpdateDeleteReaction::getType()] = [
+        'showitem' => '
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+        --palette--;;config,
+        --palette--;;interestCreateUpdateDelete,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+        --palette--;;access',
+        'columnsOverrides' => [
+            'impersonate_user' => [
+                'config' => [
+                    'minitems' => 1,
+                ],
+            ],
+        ],
+    ];
 }
