858febe026c103f47f8df9ff474b983f20ac218a
mabolek <git@mabolek.dev>
2025-10-28 09:03:10 +0100

    [CLEANUP] CS fixes

--- Classes/Command/ClearRecordHashCommandController.php
+++ Classes/Command/ClearRecordHashCommandController.php
@@ -6,7 +6,6 @@ namespace FriendsOfTYPO3\Interest\Command;
 
 use FriendsOfTYPO3\Interest\Domain\Repository\RemoteIdMappingRepository;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
